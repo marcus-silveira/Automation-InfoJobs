0b29b23839be61809d1b1c61a897b7975f1510f1
e.marcus.silveira <e.marcus.silveira@brde.com.br>
2024-01-18 13:26:15 -0300

    finalizar aplicacao da vaga

--- src/scrapers/find_jobs.py
+++ src/scrapers/find_jobs.py
@@ -9,6 +9,7 @@ class FindJobs:
     def __init__(self, url: str, driver_manager: WebDriverManager):
         self.driver: WebDriver = driver_manager.get_driver()
         self.url = url
+        self.total_pages = None
 
     def accept_cookies(self):
         self.driver.find_element(By.ID, "didomi-notice-agree-button").click()
@@ -32,14 +33,32 @@ class FindJobs:
             self.driver.find_element(By.ID, "Password").send_keys(password)
             self.driver.find_element(By.CLASS_NAME, "js_loginButton").click()
 
-    def search_jobs(self, keyword: str, home_office: bool = False):
+    def search_jobs(self, keyword: str):
         self.driver.get("https://www.infojobs.com.br/")
-        self.driver.find_element(By.XPATH, '//*[@id="keywordsCombo"]').send_keys(keyword)
+        self.driver.find_element(
+            By.XPATH, '//*[@id="keywordsCombo"]').send_keys(keyword)
         sleep(1)
 
         self.driver.find_element(By.XPATH, '//*[@id="city"]').click()
         self.driver.find_element(By.XPATH, '//*[@id="city"]').click()
         self.driver.find_element(By.XPATH, '//*[@id="city"]').clear()
         sleep(1)
-        self.driver.find_element(By.XPATH, '/html/body/main/div[1]/section/div[1]/div[4]/a').click()
-        sleep(10)
+        self.driver.find_element(
+            By.XPATH, '/html/body/main/div[1]/section/div[1]/div[4]/a').click()
+
+        self.total_pages = int(self.driver.find_element(
+            By.XPATH, '//*[@id="resumeVacancies"]/div/div[2]').text.split(" ")[-1])
+
+    def apply_jobs(self, keyword: str, home_office: bool = False):
+        self.search_jobs(keyword)
+        for page in range(1, self.total_pages):
+            if home_office:
+                self.driver.get(f"https://www.infojobs.com.br/vagas-de-emprego-{keyword}-home_office.aspx?page={page}")
+
+            self.driver.get(f"https://www.infojobs.com.br/vagas-de-emprego-{keyword}.aspx?page={page}")
+            jobs = self.driver.find_elements(By.CSS_SELECTOR, "[class='card card-shadow card-shadow-hover text-break mb-16 grid-row js_rowCard']")
+            for job in jobs:
+                print("xxxxxxxxxx")
+                job.click()
+                sleep(10)
+
